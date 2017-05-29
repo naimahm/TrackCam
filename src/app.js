f4a66e033fa9200b93c85049988a57c00ff21027
Naimah Mumin <naimahmumin@gmail.com>
2017-05-29 19:57:12 -0400

    init

---
+++ src/app.js
@@ -0,0 +1,84 @@
+var http = require('http');
+var express = require('express');
+var piServo = require('pi-servo');
+var piblaster = require('pi-blaster.js');
+var app = express();
+ 
+const tiltPin = 17, panPin = 18;
+const defaultPan = 135, defaultTilt = 90;
+var tilt = new piServo(tiltPin); 
+var pan = new piServo(panPin);
+
+app.use(express.static(__dirname));
+
+//pan/tilt to certain degree
+app.get('/:servo/:degree', function(req, res) { 
+	try{
+		console.log(req.params);
+		var servoParam = req.params.servo, degree = req.params.degree, servo = null;
+		if(servoParam && degree){
+			if(servoParam === "pan") servo = pan;
+			if(servoParam === "tilt") servo = tilt;	
+			servo.open().then(function(){  
+			  servo.setDegree(degree); // 0 - 180
+			});
+		}
+		res.end(servoParam+' servo set to '+degree);
+	}catch(e){
+		console.log("Serve moving failed", e);
+		res.end(e);
+	}
+});
+ 
+
+//turn off, specified servo
+app.get('/off/:servo', function(req, res) { 
+	try{
+		var servo = req.param.servo;
+		if(servo){
+			if(servo === "pan"){
+				pan.open().then(function(){  
+				  pan.setDegree(defaultPan); // 0 - 180
+				});
+				piblaster.setPwm(panPin, 0);
+	
+			}
+			if(servo === "tilt"){
+				tilt.open().then(function(){  
+				  tilt.setDegree(defaultTilt); // 0 - 180
+				});
+				piblaster.setPwm(tiltPin, 0);
+			}
+		}else{
+			pan.open().then(function(){  
+			  pan.setDegree(defaultPan); // 0 - 180
+			});
+			piblaster.setPwm(panPin, 0);
+			tilt.open().then(function(){  
+			  tilt.setDegree(defaultTilt); // 0 - 180
+			});
+			piblaster.setPwm(tiltPin, 0);
+		}
+		res.end('Servos have been shut down');
+	}catch(e){
+		res.end('Failed to shut servos down');		
+	}
+});
+
+//404
+app.get('*', function (req, res) {
+     res.status(404).send('Nope');
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+      if (req.xhr) {
+          res.status(500).send('Oops, Something went wrong!');
+      } else {
+          next(err);
+      }
+});
+
+app.listen(3000);
+console.log('App Server running at port 3000');
+
